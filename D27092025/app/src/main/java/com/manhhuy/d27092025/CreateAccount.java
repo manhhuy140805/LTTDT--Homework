29fbf3fa50a54de181c216d2d5a1a592e24d590c
manhhuy140805 <trandinhmanhhuy05@gmail.com>
2025-09-26 10:40:10 +0700

    init project

---
+++ D27092025/app/src/main/java/com/manhhuy/d27092025/CreateAccount.java
@@ -0,0 +1,28 @@
+package com.manhhuy.d27092025;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.manhhuy.d27092025.databinding.ActivityCreateAccountBinding;
+
+public class CreateAccount extends AppCompatActivity {
+    private ActivityCreateAccountBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        addEvent();
+    }
+
+
+}
---
+++ D27092025/app/src/main/java/com/manhhuy/d27092025/MainActivity.java
@@ -0,0 +1,28 @@
+package com.manhhuy.d27092025;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.manhhuy.d27092025.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        addEvent();
+    }
+
+
+}
